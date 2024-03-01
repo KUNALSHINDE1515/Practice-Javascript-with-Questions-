06e2d7f82d3b92183d614ad4eb5c843a1fc98d6a
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-03-01 12:23:36 +0530

    Arrays practice

---
+++ 4.Array.js
@@ -0,0 +1,61 @@
+// Basic Array Operations:
+
+// Create an array containing the names of five fruits.\
+
+const fiveFruit = ["apple", "watermelon", "banana", "orange", "watermelo"]
+// Add a new fruit to the end of the array.
+// console.log(fiveFruit.push("pine-apple"));
+// console.log(fiveFruit);
+
+// Remove the first fruit from the array.
+
+// console.log(fiveFruit.shift()); // shift method to use to remove array from frist
+// console.log(fiveFruit);
+
+// Print the third fruit in the array
+// console.log(fiveFruit[3]);
+
+
+// Array Iteration;
+
+// Create an array of numbers from 1 to 10.
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// Use a loop to print each number in the array.
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+    
+}
+
+// Create an array of strings representing days of the week.
+// Use array methods to rearrange the days so that Monday is the last day.
+// Array Filtering:
+
+// Create an array of numbers.
+// Use the filter method to create a new array containing only even numbers from the original array.
+// Array Mapping:
+
+// Create an array of numbers.
+// Use the map method to double each number in the array.
+// Array Reduction:
+
+// Create an array of numbers.
+// Use the reduce method to find the sum of all numbers in the array.
+// Nested Arrays:
+
+// Create a two-dimensional array representing a tic-tac-toe board.
+// Write a function to check if any player has won the game.
+// Array Sorting:
+
+// Create an array of strings representing names.
+// Sort the array in alphabetical order.
+// Array Searching:
+
+// Create an array of objects, each containing a name and age property.
+// Write a function to find an object in the array based on the name.
+// Array Slicing:
+
+// Create an array of numbers from 1 to 10.
+// Use the slice method to create a new array containing only the numbers from 4 to 7.
