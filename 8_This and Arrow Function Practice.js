7b536f130b459efcc676fcefd9a9885f973c5d4a
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-03-14 20:16:11 +0530

    arrow f  and this k practice

---
+++ 8_This and Arrow Function Practice.js
@@ -0,0 +1,25 @@
+// Write a regular JavaScript function called add that takes two parameters a and b, and returns the sum of a and b.
+
+function add(a ,b) {
+    return a + b;
+}
+
+let result = add(25,58)
+console.log(result);
+// Convert the add function from question 1 into an arrow function.
+
+// Write a regular JavaScript function called isEven that takes a parameter num and returns true if num is even, and false otherwise.
+
+// Convert the isEven function from question 3 into an arrow function.
+
+// Write a regular JavaScript function called greet that takes a parameter name and returns a greeting message like "Hello, [name]!".
+
+// Convert the greet function from question 5 into an arrow function.
+
+// Write a regular JavaScript function called calculateArea that takes two parameters length and width and returns the area of a rectangle (length * width).
+
+// Convert the calculateArea function from question 7 into an arrow function.
+
+// Write a regular JavaScript function called printNumbers that takes a parameter n and prints the numbers from 1 to n (inclusive) to the console.
+
+// Convert the printNumbers function from question 9 into an arrow function
