bfd2fc1554dd23757e353816432759a9dd81c168
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-06-25 17:35:49 +0530

    Assignment one

---
+++ Assignment/Assignment-1/Q2.js
@@ -0,0 +1,63 @@
+// . Create 2 valid variables and 2 invalid variables and print them onto the console. Comment the results and 
+// error messages.  
+
+// valid variables
+let name = "Kunal shinde";
+// console.log(name); // kunal shinde
+
+
+let iAmHappy = true
+// console.log(iAmHappy);  // true
+
+
+
+// invalid variables
+
+// let 1name = "deshmukh";
+// console.log(1name);     //SyntaxError: Invalid or unexpected token
+// let var = 13
+// console.log(var);  //SyntaxError: Unexpected token 'var'
+
+
+// . Write a program that prints the multiplication table in the textbook format of any number specified.  
+
+
+let number = 8;
+
+// console.log(`${8} * 1 = ${number * 1}`);
+// console.log(`${8} * 2 = ${number * 2}`);
+// console.log(`${8} * 3 = ${number * 3}`);
+// console.log(`${8} * 4 = ${number * 4}`);
+// console.log(`${8} * 5 = ${number * 5}`);
+// console.log(`${8} * 6 = ${number * 6}`);
+// console.log(`${8} * 7 = ${number * 7}`);
+// console.log(`${8} * 8 = ${number * 8}`);
+// console.log(`${8} * 9 = ${number * 9}`);
+// console.log(`${8} * 10 = ${number *10}`);
+
+
+
+//  Write a program to perform all the arithmetic operations[except increment and decrement operators] o
+
+
+let num1 = 50
+let num2 = 5
+
+// addition
+
+console.log(`Tha addition is ${num1 + num2}`);
+
+// substraction
+console.log(`Tha substraction is ${num1 - num2}`);
+
+// multiplication
+console.log(`Tha multiplication is ${num1 * num2}`);
+
+// division
+console.log(`Tha division is ${num1 / num2}`);
+
+// Modules(%) remider in marathi baki
+console.log(`Tha Modules is ${num1 % num2}`);
+
+// Exponentiation
+console.log(`Tha Exponentiation is ${num1 ** num2}`);
---
+++ Assignment/Assignment-1/assignment.js
@@ -0,0 +1,81 @@
+
+
+// declaration string
+
+let check = "I am so happy to join Pw Sills FSWD Course";
+
+// array
+
+
+let ecommercePurchase = [
+    "iphone",
+    "MAcbook Pro",
+    "Flower Pot",
+    "Water Bottle",
+   
+    "Keyboard", "Mac Studio mini",
+    "Watch",
+    "Tennis Ball",
+    "Mouse PAd",
+    "Lens"
+];
+
+// console.log(ecommercePurchase);
+
+const registerStudent = {
+    registrstion_Number_1:"Mithun",
+    registrstion_Number_2:"Alka",
+    registrstion_Number_3:"Anurag",
+    registrstion_Number_4:"Prabir",
+    registrstion_Number_5:"Shivam",
+
+}
+
+// console.log(registerStudent);
+
+
+
+// checking variable type of
+
+let var1 = "I am so happy to learn fullstack development";
+// console.log(typeof var1);  // type of var1 is string
+
+let var2 = 9;
+// console.log(typeof var2);  // output:number
+
+let var3 = 8.4;
+// console.log(typeof var3);  // number
+
+let var4 = Number.POSITIVE_INFINITY;
+// console.log(typeof var4);  // number
+
+let var6 = NaN
+// console.log(typeof var6);  // number
+
+let var7 = 1024n
+// console.log(typeof var7);  // BigInt
+
+let var8 = true; 
+// console.log(typeof var8);   // boolean
+
+let var9 = undefined; 
+// console.log(typeof var9); // undefined
+
+let var10 = null; 
+// console.log(typeof var10); //  here type of object
+
+let var11 = Symbol; 
+// console.log(typeof var11);  // here output are function
+
+let var12 = [ 
+    "Mac Studio mini",
+    "Watch",
+    "Tennis Ball",
+    "Mouse PAd"
+]; 
+// console.log(typeof var12);  // here type of object
+// console.log(typeof registerStudent);  // here also outut is object
+
+
+
+
