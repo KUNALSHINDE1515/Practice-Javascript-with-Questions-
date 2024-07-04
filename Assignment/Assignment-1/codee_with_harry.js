f88aa4238d8013b0372dcacb9dfecc7f3f9192e4
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-04 08:56:08 +0530

    basic c-1

---
+++ Assignment/Assignment-1/codee_with_harry.js
@@ -0,0 +1,16 @@
+let variable = "kunal"
+
+let result = variable + 3
+console.log( typeof result);
+
+
+const obj = {
+    number:9328544
+}
+obj.number = "change"
+obj.name = "shinde"
+console.log(obj);
+
+// *************************************************************Experation in js*********************************
+
+
