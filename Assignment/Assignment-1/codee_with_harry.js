49b7e81d027fffcabee9bd1de6d2791d6dcc0612
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-04 09:08:43 +0530

    basic - 2

--- Assignment/Assignment-1/codee_with_harry.js
+++ Assignment/Assignment-1/codee_with_harry.js
@@ -13,4 +13,12 @@ console.log(obj);
 
 // *************************************************************Experation in js*********************************
 
+let age = 920
 
+if(age < 10){
+    console.log(`You age is less than required age `);
+}else if(age > 20){
+    console.log(`You age is greater than required age `);
+}else{
+    console.log(`You are in 10 to 20 age`);
+}
