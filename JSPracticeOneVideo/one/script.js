407c13c6b56de4b13676aaa90e836ffbebee8ca7
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-08-01 19:57:51 +0530

    practice js

---
+++ JSPracticeOneVideo/one/script.js
@@ -0,0 +1,66 @@
+// static number program
+
+// // prompt()
+// let a = prompt("please enter frist number: ")
+// let b = prompt("please enter second number: ")
+// c = a + b
+
+
+
+// square root of number
+
+
+let d  = 81
+let e = Math.sqrt(d)
+console.log(e);
+
+
+// calculate area of trangle
+
+
+let base = 5
+let height = 10
+
+// area = base * height / 2
+// console.log(`${base} and ${height}  result: ${area} `);
+
+
+// 2nd method
+
+
+let a1 = 10
+let b1 = 10
+let c1 = 10
+let s = (a1 + b1 + c1) / 2 
+// console.log(s);
+
+let temp = s * (s - a1) * (s - b1) * (s - c1)
+let area = Math.sqrt(temp)
+// console.log(area);
+
+
+
+// swap two number
+
+// 1st method
+let a2 = 21
+let b2 = 24 
+
+// console.log(a2 , b2);
+// let tems = a2;
+// a2 = b2;
+// b2 = tems;
+
+
+//2 nd method
+
+a2 = a2 + b2
+b2 = a2 - b2
+a2 = a2 - b2
+// console.log(a2 , b2);
+
+
+
+//convert celsius tp fahreint js function
+
+
