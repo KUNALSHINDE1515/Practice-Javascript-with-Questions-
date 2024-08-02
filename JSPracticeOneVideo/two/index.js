db135b662947907984b7e2f11210c076feeb0cd2
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-08-02 11:31:15 +0530

    practice js

---
+++ JSPracticeOneVideo/two/index.js
@@ -0,0 +1,21 @@
+// odd or even
+
+
+function oddEven(nums) {
+    
+    if (nums % 2 == 0) {
+        console.log('number is even');
+    }else{
+        console.log('num is odd');
+    }
+}
+
+// oddEven(9)
+
+let x = 4
+let res = x % 2 == 0 ? "Even" : 'Odd'
+console.log(`${x} is an ${res}`);
+
+
+// prime
+
